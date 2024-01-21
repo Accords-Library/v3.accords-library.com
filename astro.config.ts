1f448f14f0dd9c0774d892aae927b6c8a149ba47
DrMint <29893320+DrMint@users.noreply.github.com>
2024-01-21 14:42:52 +0100

    Updated

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
+import astroMetaTags from "astro-meta-tags";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
     mode: "standalone",
   }),
   integrations: [
+    astroMetaTags(),
     icon({
       include: {
         "material-symbols": ["*"], // Loads entire Material Design Icon set
