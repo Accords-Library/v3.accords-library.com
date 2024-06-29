246cdb4af0a93de7618b099aa12f96ebd962c1ad
DrMint <29893320+DrMint@users.noreply.github.com>
2024-06-29 13:56:46 +0200

    Fixed If modfied since logic

--- src/middleware/pageCaching.ts
+++ src/middleware/pageCaching.ts
@@ -8,10 +8,14 @@ export const pageCachingMiddleware = defineMiddleware(async ({ url, request, loc
   const cachedPage = pageCache.get(pathname);
 
   if (cachedPage) {
-    const modifiedSince = request.headers.get("If-Modified-Since");
-    const lastModified = cachedPage.headers.get("Last-Modified");
+    const clientTimestamp = request.headers.get("If-Modified-Since");
+    const serverTimestamp = cachedPage.headers.get("Last-Modified");
 
-    if (modifiedSince && lastModified && new Date(modifiedSince) <= new Date(lastModified)) {
+    if (
+      clientTimestamp &&
+      serverTimestamp &&
+      new Date(clientTimestamp) == new Date(serverTimestamp)
+    ) {
       return new Response(null, { status: 304, statusText: "Not Modified" });
     }
 
